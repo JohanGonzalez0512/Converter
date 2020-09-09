77da861ab1b9faa39a5ced220d8930d9efdb6ad1
Johan Gonzalez <Jjopi20101@gmail.com>
2020-09-09 18:37:14 -0500

    Agregando carpeta docs

---
+++ docs/precache-manifest.e1a35189e29b088a0abcbb6b6255e96c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0c4607318f0a6df04c66c06170ca93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f317aa0feac1b0df3be",
+    "url": "/static/js/2.d7752432.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7752432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28076db69ee54624625",
+    "url": "/static/js/main.7cb5f6b3.chunk.js"
+  },
+  {
+    "revision": "898d359fb45ae796227b",
+    "url": "/static/js/runtime-main.fc7ca8b7.js"
+  }
+]);
